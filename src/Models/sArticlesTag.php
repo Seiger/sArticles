a2b75e2cb1ef31426ab2e0ad7d44b0c0c2550bb1
Serhii Korneliuk <seiger@meta.ua>
2023-07-18 01:21:04 +0300

    [ADD] Tag Articles.

--- src/Models/sArticlesTag.php
+++ src/Models/sArticlesTag.php
@@ -1,8 +1,17 @@
 <?php namespace Seiger\sArticles\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Seiger\sArticles\Models\sArticle;
 
 class sArticlesTag extends Model
 {
     protected $primaryKey = 'tagid';
+
+    /**
+     * Get the Articles for the Tag.
+     */
+    public function articles()
+    {
+        return $this->belongsToMany(sArticle::class, 's_article_tags', 'tag', 'article');
+    }
 }
